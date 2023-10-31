fc2e60f76cd3ba625281ebbdae9e66e976940575
xnm-sdksdk <n.mendonca@protonmail.com>
2023-10-31 19:22:02 +0000

    closing functions and begin draw

--- script.js
+++ script.js
@@ -4,11 +4,17 @@ const ctx = canvas.getContext("2d");
 // Array para guardar os átomos
 const atoms = [];
 
-class Atomo{
+class Atomo {
+  draw(x, y, radius, color) {
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, Math.PI * 2);
+    ctx.fillStyle = "#000";
+    ctx.fill();
+    ctx.stroke();
+    ctx.closePath();
+  }
 
-  draw()
-
-  update()
+  update() {}
 }
 
-function init()
+function init() {}
