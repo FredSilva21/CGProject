1147361f5fb1471d16149a99f22906b1caa359c8
xnm-sdksdk <n.mendonca@protonmail.com>
2023-11-02 11:47:16 +0000

    draw particulas

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
 //Elementos do Canvas
 const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const ctx = canvas.getContext("2d");
 
-//Botões 
-const comecar=document.getElementById("comecar")
-const recomecar=document.getElementById("recomecar")
+//Botões
+const comecar = document.getElementById("comecar");
+const recomecar = document.getElementById("recomecar");
 
-comecar.addEventListener("click",function(){
-  if(comecar.textContent="Começar"){
-    comecar.textContent="Parar"
-    recomecar.disabled=false
-  }else{
-    comecar.textContent="Parar"
+comecar.addEventListener("click", () => {
+  if ((comecar.textContent = "Começar")) {
+    comecar.textContent = "Parar";
+    recomecar.disabled = false;
+  } else {
+    comecar.textContent = "Parar";
   }
-  
-})
+});
 
 // Array para guardar os átomos
 const atoms = [];
+
+// Class para o circulo que contem as particulas
 class Atomo {
-  constructor(x, y, raioNucleo, corNucleo, numeroEletrons, raioOrbita, corEletrons) {
+  constructor(
+    x,
+    y,
+    raioNucleo,
+    corNucleo,
+    numeroEletrons,
+    raioOrbita,
+    corEletrons
+  ) {
     this.x = x; // Coordenada x do centro do átomo no canvas
     this.y = y; // Coordenada y do centro do átomo no canvas
     this.raioNucleo = raioNucleo; // Raio do núcleo
@@ -32,7 +43,28 @@ class Atomo {
     this.eletrons = []; // Array para armazenar informações sobre os elétrons
   }
 
-  draw(){
+  draw() {}
+}
 
+// class para as particulas no interior do circulo
+class Particulas {
+  constructor(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
+    this.radius = Math.random() * 20;
+    this.dx = Math.random() * 2;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+    ctx.strokeStyle = "#000";
+    ctx.stroke();
+    ctx.fill();
+  }
+
+  move() {
+    this.x = Math.random() * this.x + this.dx;
+    this.y = Math.random() * this.y - this.dx;
   }
 }
