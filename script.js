0888d9d829a16b49d1bbc500d4e1068a3a4f1776
FredSilva21 <fredsilva43@gmail.com>
2023-11-06 23:26:50 +0000

    start menu animation

--- script.js
+++ script.js
@@ -1,129 +1,64 @@
-// Canvas Element
+const startButton = document.getElementById("startButton");
+const restartButton = document.getElementById("restartButton");
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
 
-// DOM Elements Retrieval
-const start = document.getElementById("start");
-const restart = document.getElementById("restart");
-const temperatureRangeInput = document.getElementById("temperatureRangeInput");
-const temperatureValueDisplay = document.getElementById(
-  "temperatureValueDisplay"
-);
-const simulation = document.getElementById("simulation");
-const menuAccess = document.getElementById("menuAccess");
+let animationFrameId;
+let circles = [];
 
-// Array to store the atoms
-const atoms = [];
-// Defining the starting value
-let temperature = 1;
+let simulationRunning = false;
+let electronSpeed = 50;
+let protonCount = 1; // Set an initial proton count
+let atomCount = 0;
 
-// Boolean to check whether the simulation is running or not
-let simulationOnGoing = false;
-
-// Evaluate the value of the range input | Functional
-temperatureRangeInput.addEventListener("input", () => {
-  temperature = temperatureRangeInput.value;
-  temperatureValueDisplay.textContent = temperature;
-  console.log(temperature);
-});
-
-// start.addEventListener("click", () => {
-//   menuAccess.display = "none";
-//   simulation.style.display = "block";
-//   start.styles.display = "none";
-
-//   if (!simulationOnGoing) {
-//     simulationOnGoing = true;
-//   }
-// });
-
-start.addEventListener("click", () => {
-  if (!simulationOnGoing) {
-    // startSimulation();
-    start.style.display = "none";
-    restart.style.display = "block";
-    simulationOnGoing = true;
+startButton.addEventListener("click", () => {
+  if (!simulationRunning) {
+    startSimulation();
+    startButton.style.display = "none";
+    restartButton.style.display = "block";
+    simulationRunning = true;
   }
 });
 
-restart.addEventListener("click", () => {
-  if (simulationOnGoing) {
-    // resetSimulation();
-    start.style.display = "block";
-    restart.style.display = "none";
-    temperatureRangeInput.style.display = "none";
-    temperatureValueDisplay.style.display = "none";
-    simulationOnGoing = false;
+restartButton.addEventListener("click", () => {
+  if (simulationRunning) {
+    resetSimulation();
+    startButton.style.display = "block";
+    restartButton.style.display = "none";
+    simulationRunning = false;
   }
 });
 
-const createAtoms = (e) => {};
 
-// Class para o circulo que contem as particulas
-class Atomo {
-  constructor(
-    x,
-    y,
-    radius,
-    corNucleo,
-    numeroEletrons,
-    raioOrbita,
-    corEletrons
-  ) {
-    this.x = x; // Coordenada x do centro do átomo no canvas
-    this.y = y; // Coordenada y do centro do átomo no canvas
-    this.radius = radius; // Radius do núcleo
-    this.corNucleo = corNucleo; // Cor do núcleo
-    this.numeroEletrons = numeroEletrons; // Número de elétrons
-    this.raioOrbita = raioOrbita; // Raio da órbita dos elétrons
-    this.corEletrons = corEletrons; // Cor dos elétrons
-
-    this.nucleo = { x: this.x, y: this.y }; // Posição do núcleo
-    this.eletrons = []; // Array para armazenar informações sobre os elétrons
-  }
-
-  draw() {}
+function drawCircle(x, y) {
+  ctx.fillStyle = "red";
+  ctx.beginPath();
+  ctx.arc(x, y, 20, 0, 2 * Math.PI);
+  ctx.fill();
 }
 
-// Class for the particles inside the circle
-class Particle {
-  constructor(
-    canvas,
-    temperatureRangeInput,
-    temperatureValueDisplay,
-    containerRadius
-  ) {
-    this.canvas = canvas;
-    this.temperatureRangeInput = temperatureRangeInput;
-    this.temperatureValueDisplay = temperatureValueDisplay;
-    this.containerRadius = containerRadius;
+function animateCircles() {
+  requestAnimationFrame(animateCircles);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  for (let i = 0; i < circles.length; i++) {
+      const circle = circles[i];
+      circle.x += (circle.targetX - circle.x) * 0.1;
+      circle.y += (circle.targetY - circle.y) * 0.1;
+      drawCircle(circle.x, circle.y);
   }
-
-  createParticles() {
-    const centerX = this.canvas.width / 2;
-    const centerY = this.canvas.height / 2;
-    const radius = 10;
-    const temperature = this.temperatureRangeInput.value;
-    const speed = temperature / 10;
-    const angle = Math.random() * 2 * Math.PI;
-    const color = Math.random() * 1 ? "blue" : "orange";
-
-    const x = centerX + Math.cos(angle) * (this.containerRadius - radius);
-    const y = centerY + Math.sin(angle) * (this.containerRad - radius);
-
-    atoms.push(new Particle(x, y, radius, speed, color, this.containerRadius));
-  }
-
-  draw() {}
-
-  move() {}
 }
 
-// Click event to place the atoms
-canvas.addEventListener("click", createAtoms);
+canvas.addEventListener("mousemove", (event) => {
+  if (circles.length < 8) {
+      const circle = {
+          x: event.clientX,
+          y: event.clientY,
+          targetX: event.clientX,
+          targetY: event.clientY,
+      };
+      circles.push(circle);
+  }
+});
 
-const animate = () => {};
-
-animate();
+animateCircles();
