8673836ac03412da6f015b56d833415b93dadbb7
FredSilva21 <fredsilva43@gmail.com>
2023-11-07 00:09:29 +0000

    do menu template and starting circles animation

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ const restartButton = document.getElementById("restartButton");
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+window.addEventListener("load", followMouse());
+
 let animationFrameId;
-let circles = [];
+let menuCircles = [];
 
 let simulationRunning = false;
 let electronSpeed = 50;
@@ -29,7 +31,7 @@ restartButton.addEventListener("click", () => {
   }
 });
 
-
+//Menu Functions
 function drawCircle(x, y) {
   ctx.fillStyle = "red";
   ctx.beginPath();
@@ -37,28 +39,45 @@ function drawCircle(x, y) {
   ctx.fill();
 }
 
+for (let i = 0; i < 10; i++) {
+  const x = Math.random() * canvas.width;
+  const y = Math.random() * canvas.height;
+  menuCircles.push({ x, y });
+}
+
 function animateCircles() {
   requestAnimationFrame(animateCircles);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  for (let i = 0; i < circles.length; i++) {
-      const circle = circles[i];
-      circle.x += (circle.targetX - circle.x) * 0.1;
-      circle.y += (circle.targetY - circle.y) * 0.1;
-      drawCircle(circle.x, circle.y);
+  for (let i = 0; i < menuCircles.length; i++) {
+    const circle = menuCircles[i];
+    drawCircle(circle.x, circle.y);
   }
 }
+function followMouse() {
+  canvas.addEventListener("mousemove", (event) => {
+    for (let i = 0; i < menuCircles.length; i++) {
+      const circle = menuCircles[i];
+      const dx = event.clientX - circle.x;
+      const dy = event.clientY - circle.y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
 
-canvas.addEventListener("mousemove", (event) => {
-  if (circles.length < 8) {
-      const circle = {
-          x: event.clientX,
-          y: event.clientY,
-          targetX: event.clientX,
-          targetY: event.clientY,
-      };
-      circles.push(circle);
-  }
-});
+      if (distance > 10) {
+        const angle = Math.atan2(dy, dx);
+        const moveDistance = distance - 10; // Move the circle 10 pixels closer to the mouse
+        const moveX = Math.cos(angle) * moveDistance * 0.05; // Adjust the 0.05 value for smoother transition
+        const moveY = Math.sin(angle) * moveDistance * 0.05; // Adjust the 0.05 value for smoother transition
+
+        // Update the circle's position
+        circle.x += moveX;
+        circle.y += moveY;
+      }
+    }
+  });
+}
 
 animateCircles();
+
+function startSimulation() {
+  console.log("TESTE");
+}
