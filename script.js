17306ffd49f59286951650354f3c733ebd059ee1
FredSilva21 <fredsilva43@gmail.com>
2023-11-13 16:26:18 +0000

    some animations and main page render

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Menu
 window.addEventListener("DOMContentLoaded", function () {
   // Create additional balls dynamically
   const numBalls = 20; // Adjust the number of balls
@@ -12,11 +13,56 @@ window.addEventListener("DOMContentLoaded", function () {
   }
 });
 
+const start = document.getElementById("startButton");
+start.addEventListener("click", function () {
+  let background = document.querySelector(".background");
+  background.style.display = "none";
+  document.body.innerHTML = `
+    <canvas id="canvas"></canvas>
+    <div id="simulation">
+      <!--Temperature-->
+      <div id="temperature"><label for="temperatureRangeInput">Temperature:</label>
+        <input
+          type="range"
+          max="100"
+          min="0"
+          step="1"
+          value="50"
+          id="temperatureRangeInput"
+        />
+        <span id="temperatureValueDisplay">50</span>
+      </div>
+      <button type="button" id="restart" disabled>Restart</button>
+    </div>`;
+
+  const temperatureRangeInput = document.getElementById("temperatureRangeInput");
+  const temperatureValueDisplay = document.getElementById("temperatureValueDisplay");
+  const temperatureLabel = document.querySelector("#temperature label");
+
+  temperatureRangeInput.addEventListener("input", function () {
+    temperatureValueDisplay.textContent = temperatureRangeInput.value;
+    if (temperatureRangeInput.value > 50) {
+      temperatureLabel.style.color = "orange";
+      temperatureValueDisplay.style.color="orange"
+    }else if(temperatureRangeInput.value < 50){
+      temperatureLabel.style.color = "blue";
+      temperatureValueDisplay.style.color="blue"
+    }else{
+      temperatureLabel.style.color = "white";
+      temperatureValueDisplay.style.color="white"
+    }
+    
+  });
+});
+
+
+
+
+//Canvas
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-const W = canvas.width,
-  H = canvas.height;
-
+const W = screen.width,
+  H = screen.height;
 function startSimulation() {
   const particles = [];
 
@@ -85,7 +131,6 @@ let electrons = {
   },
 };
 
-
 let circle = {
   // Drawing the outer circle
   drawCircle(x, y, radius) {
@@ -93,16 +138,15 @@ let circle = {
     ctx.arc(x, y, radius, 0, 2 * Math.PI);
     ctx.strokeStyle = "rgba(255,255,255)";
     ctx.stroke();
-  }
-  ,
+  },
   // Drawing the inner circle
   drawCircle2(x, y, radius) {
     ctx.beginPath();
     ctx.arc(x, y, radius, 0, 2 * Math.PI);
     ctx.strokeStyle = "rgba(255,255,255)";
     ctx.stroke();
-  }
-}
+  },
+};
 // For loop to generate several electrons
 function drawElectrons() {
   for (let i = 0; i < electrons.numElectrons; i++) {
@@ -117,7 +161,6 @@ function drawElectrons2() {
     electrons.ang += 360 / electrons.numElectronsInside;
   }
 }
-
 
 function drawProtons() {
   console.log("Protons");
